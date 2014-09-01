efe0a12c7e33ee1a691e970b5d3bc74f556827a8
Daniel Wehner <daniel.wehner@erdfisch.de>
2014-09-01 14:03:13 +0200

    fix the plugin manager

--- src/InlineEntityFormPluginManager.php
+++ src/InlineEntityFormPluginManager.php
@@ -16,7 +16,7 @@ class InlineEntityFormPluginManager extends DefaultPluginManager {
 
   public function __construct(\Traversable $namespaces, CacheBackendInterface $cache_backend, LanguageManager $language_manager, ModuleHandlerInterface $module_handler) {
     parent::__construct('Plugin/InlineEntityForm', $namespaces, $module_handler);
-    $this->setCacheBackend($cache_backend, $language_manager, 'inline_entity_form_plugins');
+    $this->setCacheBackend($cache_backend, 'inline_entity_form_plugins');
   }
 
 } 
